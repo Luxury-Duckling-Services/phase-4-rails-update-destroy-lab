f7aff9218b387aaf4f6970b63e6d6828c9cd35f4
Yifan <datasciencewyfan@gmail.com>
2022-07-21 14:14:11 -0700

    commit tree 754feb0ec2ac8d5d5365302b5ffaf2298cb4e659

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,7 @@
 class PlantsController < ApplicationController
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   # GET /plants
   def index
     plants = Plant.all
@@ -18,9 +20,34 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+  def change_is_in_stock
+    plant = Plant.find_by(id: params[:id])
+    plant.update(is_in_stock: !plant.is_in_stock)
+    render json: plant
+  end
+
+  # PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  # DELETE /plant/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
   end
 end
